6b0691c1582b3bdbd65e74e427155d541a801039
sabjj <lol.ru.sabj@gmail.com>
2014-11-20 17:58:16 +0300

    life

--- test.rb
+++ test.rb
@@ -3,10 +3,30 @@ require 'life_for_site'
 
  
 set :public_folder, File.dirname(__FILE__) + '/static'
+	
+	@@a = Life_for_site.new
+	@@n = 10
+	@@m = 10
+	@@life_map = @@a.start_life(@@n,@@m)
+	@@gen = 0
+	@@life_prev = []
 
-get '/life', :provides => 'html' do 
-	@a = Life_for_site.new
-	@n = 10
-	@m = 10
+get '/', :provides => 'html' do 
+	erb :site
+end
+
+get '/game_over' do
+	erb :game
+end
+
+post '/' do
+	@@n = params[:n_count].to_i
+	@@m = params[:m_count].to_i
+	@@life_map = @@a.start_life(@@n,@@m)
+	erb :site
+end
+
+post '/life' do
+	@@life_map = @@a.next_step()
 	erb :site
 end
